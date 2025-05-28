d74af2fdd1e7758d6eb35d08fa3e34fe238b421c
ernaud-breissie <ernaudbreissie@gmail.com>
2025-05-28 15:43:52 +0200

    wip main Updated: 2025-05-28 15:43:52

--- commit.sh
+++ commit.sh
@@ -13,6 +13,14 @@ pip freeze > request.txt
 # Get current branch
 branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
 
+# Store original remote URL
+original_url=$(git remote get-url origin)
+
+# Configure remote URL with token authentication
+repo_path=$(echo "$original_url" | sed -e 's/https:\/\/github.com\///' -e 's/git@github.com://')
+auth_url="https://$login:$github_token@github.com/$repo_path"
+git remote set-url origin "$auth_url"
+
 # Pull latest changes
 git pull origin $branch
 
@@ -26,6 +34,9 @@ git commit -m "wip $branch $msg"
 
 # Push changes
 git push origin $branch
+
+# Restore original remote URL without credentials
+git remote set-url origin "https://github.com/$repo_path"
 
 # Restore normal git configuration
 git config --local user.name "$user_normal"
