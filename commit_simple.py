a1d6287fc880b911c99a29325e35f0449ed3f310
ernaud-breissie <ernaudbreissie@gmail.com>
2025-05-28 20:14:04 +0200

    wip main Updated: 2025-05-28 20:14:04

--- commit_simple.py
+++ commit_simple.py
@@ -115,8 +115,8 @@ def restore_git_config(backup_path):
     with open('.git/config', 'w') as f:
         f.write(config)
         # restore the origin user and email
-    run_cmd(['git', 'config', '--local', 'user.name', os.getenv('user_normal')], check=True)
-    run_cmd(['git', 'config', '--local', 'user.email', os.getenv('email_normal')], check=True)
+    run_cmd(['git', 'config', '--global', 'user.name', os.getenv('user_normal')], check=True)
+    run_cmd(['git', 'config', '--global', 'user.email', os.getenv('email_normal')], check=True)
 
 def check_env_variables():
     """Check if all required environment variables are set."""
